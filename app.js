50292732c44f07223fc2b2611aac093b5c898fdd
sanath poojari <sanathpoojari@outlook.com>
2024-03-31 11:22:47 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Git practic'));
+app.get('/', (req, res) => res.send('Completed the pipeline creation'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
